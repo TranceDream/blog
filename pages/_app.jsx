41a1185887f081487d43475b1303fabf26d258f4
TranceDream <xgxx111@outlook.com>
2022-10-02 10:32:27 +0800

    [✨feature] Replace page transition with nprogress

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,28 +4,21 @@ import '@fontsource/roboto'
 import 'katex/dist/katex.min.css'
 import 'react-toastify/dist/ReactToastify.min.css'
 import { ToastContainer } from 'react-toastify'
-import { AnimatePresence, motion } from 'framer-motion'
+import nprogress from 'nprogress'
 import Header from '../components/Header'
+import { Router } from 'next/router'
 
-function MyApp({ Component, pageProps, router }) {
+Router.events.on('routeChangeStart', nprogress.start)
+Router.events.on('routeChangeError', nprogress.done)
+Router.events.on('routeChangeComplete', nprogress.done)
+
+function MyApp({ Component, pageProps }) {
     return (
-        <AnimatePresence initial>
+        <>
             <Header logo={pageProps.logo} />
-            <motion.div
-                key={router.route}
-                initial='hidden'
-                animate='enter'
-                exit='exit'
-                transition={{ type: 'linear' }}
-                variants={{
-                    hidden: { opacity: 0 },
-                    enter: { opacity: 1 },
-                    exit: { opacity: 0 },
-                }}>
-                <Component {...pageProps} />
-            </motion.div>
+            <Component {...pageProps} />
             <ToastContainer />
-        </AnimatePresence>
+        </>
     )
 }
 
