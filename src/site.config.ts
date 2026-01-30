e9ad30cf15fafa9eaecc1aa3c5a52053bfbb76b6
TranceDream <xgxx111@outlook.com>
2026-01-30 16:43:25 +0800

    Update posts

--- src/site.config.ts
+++ src/site.config.ts
@@ -174,10 +174,8 @@ const config: SiteConfig = {
   // Make sure to compress the image to a web-friendly size (<100kb)
   // Try using the excellent https://squoosh.app web app for creating small webp files
   characters: {
-    // owl: '/owl.webp',
-    // unicorn: '/unicorn.webp',
-    // duck: '/duck.webp',
     pusheen: '/pusheen.webp',
+    'handshake-pusheen': '/handshake-pusheen.webp',
   },
 }
 
